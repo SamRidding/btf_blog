afc8c35d2cb2063d5a9116cdfebca18ade1e2017
Sam Ridding <sam.ridding@hotmail.co.uk>
2024-02-08 22:40:55 +0000

    Register Post model for admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'slug', 'mix', 'posted_on')
+    search_fields = ['title', 'content']
+    list_filter = ('draft', 'posted_on')
+    prepopulated_fields = {'slug': ('title',)}
