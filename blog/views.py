d635a155e7b6eab72f30e7d53ed5673d287230a7
Sam Ridding <sam.ridding@hotmail.co.uk>
2024-02-09 13:11:04 +0000

    Add blog class view to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic, View
 
-# Create your views here.
+from .models import Post
+
+class Blog(generic.ListView):
+    """View to display posts on blog page"""
+
+    model = Post
+    template_name = "blog/blog.html"
