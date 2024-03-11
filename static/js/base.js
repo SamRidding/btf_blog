6a4463a2ae8260b2322a2b377e85a1834526ced4
Sam Ridding <sam.ridding@hotmail.co.uk>
2024-03-11 19:34:18 +0000

    Add js folder with toggleSearch function

---
+++ static/js/base.js
@@ -0,0 +1,8 @@
+function toggleSearch() {
+    var search = document.getElementById('search');
+    if (search.style.display === "flex") {
+      search.style.display = "none"
+    } else {
+      search.style.display = "flex"
+    }
+  }
