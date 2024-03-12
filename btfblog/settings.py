5f0340fc1da608140ac55b19596c883e0fafd24d
Sam Ridding <sam.ridding@hotmail.co.uk>
2024-03-12 21:31:11 +0000

    Update allowed hosts & trusted origins

--- btfblog/settings.py
+++ btfblog/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
+ALLOWED_HOSTS = ['.backtofront.blog', '.railway.app','.localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://*.railway.app', 'https://www.backtofront.blog']
 
 # Application definition
 
