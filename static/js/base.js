ea56c457130ddc299ee49e9993fdfb28d2cc08d9
Sam Ridding <sam.ridding@hotmail.co.uk>
2024-03-12 22:01:25 +0000

    Add togglecontact fuction to base.js

--- static/js/base.js
+++ static/js/base.js
@@ -6,3 +6,13 @@ function toggleSearch() {
       search.style.display = "flex"
     }
   }
+
+
+function toggleContact() {
+  var search = document.getElementById('contact');
+  if (search.style.display === "flex") {
+    search.style.display = "none"
+  } else {
+    search.style.display = "flex"
+  }
+}
