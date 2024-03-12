51be2fc513f1e5cae26a647c9f9cd2451e5af65a
Sam Ridding <sam.ridding@hotmail.co.uk>
2024-03-12 21:11:13 +0000

    Add url path for blog search

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
      path("", views.Blog.as_view(), name="blog"),
+     path("search/", views.search, name="search"),
      path('<slug:slug>/', views.BlogPost.as_view(), name='blog_post'),
      path('genre/<slug:tag_slug>/', views.GenreFilter.as_view(), name='filter_genre'),
 ]
